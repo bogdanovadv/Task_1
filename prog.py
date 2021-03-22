fbf45a4eac5760f38158496993d5387c0aade1bc
Darya Bogdanova <daxren.db@gmail.com>
2021-03-23 00:43:06 +0500

    1/

--- prog.py
+++ prog.py
@@ -1,4 +1,4 @@
 letters = ('a', 'o', 'u', 'i', 'e', 'y')
-string = input("Введите строку: ")
+string = input("Введите строку: ").lower()
 for i in letters:
-    print(i, string.count(i), end=', ')
+    print(i, string.count(i), end=", " if letters[-1] != i else "")
